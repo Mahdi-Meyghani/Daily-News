6ccda4d6c13705e129335b10d859ba9b4dee2ac6
Mahdi <mahdimeyghani02@gmail.com>
2024-05-18 16:02:16 +0330

    Add subject, specify language, limit news and more control to topic

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from send_email import send_email
 
 API_KEY = "8982b46221af4410ba4a67eb9a0a71a9"
 
-url = ("https://newsapi.org/v2/everything?q=spacex&from="
-       "2024-04-17&sortBy=popularity&apiKey="
-       "8982b46221af4410ba4a67eb9a0a71a9")
+topic = "AI"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2024-05-17&"
+       "sortBy=popularity&"
+       "language=en&"
+       "apiKey=8982b46221af4410ba4a67eb9a0a71a9")
 
 # Send request
 response = requests.get(url)
@@ -16,9 +20,13 @@ content = response.json()
 
 # Access to the article titles and descriptions
 msg = ""
-for article in content["articles"]:
+
+for article in content["articles"][0:20]:
     if article["title"] and article["title"] != "[Removed]":
         msg += f"{article['title']}\n{article['description']}\n{article['url']}\n\n"
 
+
+msg = f"Subject: Today's NEWS\n\n{msg}"
 msg = msg.encode("utf-8")
+
 send_email(msg)
